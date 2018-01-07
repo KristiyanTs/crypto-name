ff5a000a4c69feb6f2c3ffaaa7612cd8f4562eba
Kristiyan96 <kristiyan.c@gmail.com>
2018-01-07 18:59:43 +0200

    Edited user details

--- db/migrate/20171116201857_create_users.rb
+++ db/migrate/20171116201857_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
