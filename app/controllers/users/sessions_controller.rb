1e840e653faf59f4e0ee86cc96151ba2ab2e8d1b
Kristiyan96 <kristiyan.c@gmail.com>
2018-01-08 15:20:57 +0200

    Ajax log in and register

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,26 +6,38 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+def create
+  resource = User.find_for_database_authentication(email: params[:user][:email])
+  return invalid_login_attempt unless resource
+
+  if resource.valid_password?(params[:user][:password])
+    sign_in :user, resource
+    return render nothing: true
+  end
+
+  invalid_login_attempt
+ end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-
+  
   private 
 
   def after_sign_in_path_for(resource)
     search_index_path
   end
+
+  protected
+
+  def invalid_login_attempt
+    set_flash_message(:alert, :invalid)
+    render json: flash[:alert], status: 401
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
