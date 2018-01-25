7bea2b58a3d70e37bdaa832eed7ff2c13011c7ac
Kristiyan96 <kristiyan.c@gmail.com>
2018-01-25 17:01:06 +0200

    Fix details form

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,6 @@ class CartsController < ApplicationController
   before_action :login
 
   def show
-    @right_wrapper = 'details/info'
     @detail = current_user.detail || Detail.new
     @price = client_cart.total
   end
