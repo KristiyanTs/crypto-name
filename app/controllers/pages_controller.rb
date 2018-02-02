3cad50475bc2700a2a3cbb5cab598fcc6d690e57
Kristiyan96 <kristiyan.c@gmail.com>
2018-02-02 10:39:38 +0000

    Chnages in this branch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def show
+    @right_wrapper = 'pages/terms_nav' if params[:page] == 'terms'
     render template: "pages/#{params[:page]}"
   end
 end
