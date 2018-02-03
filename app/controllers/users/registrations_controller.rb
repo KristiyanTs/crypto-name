2da307299e2700dbd6ae906cf4b07daaf462a9d1
Kristiyan <kristiyan.c@gmail.com>
2018-02-03 16:46:32 +0000

    Home page (#42)
    * Fix sidenav height on mobile
    
    * Chnages in this branch

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+  def edit
+    @right_wrapper = 'devise/registrations/profile_nav'
+    super
+  end
+
   def create
     build_resource(sign_up_params)
 
