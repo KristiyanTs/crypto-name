261e2725efd11e76507ecaa503708d2f320b31cb
Kristiyan96 <kristiyan.c@gmail.com>
2018-02-20 15:43:51 +0200

    Added google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Google analytics tracker
+  GA.tracker = "UA-59328904-5"
 end
