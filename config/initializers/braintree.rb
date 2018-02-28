d44bc93e4a8dcbe1d983128ace8a8c302da632c1
Kristiyan <kristiyan.c@gmail.com>
2018-02-28 15:33:47 +0200

    Update braintree.rb

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-BraintreeRails::Configuration.environment = Rails.env.production? ? :production : :sandbox
-BraintreeRails::Configuration.logger = Logger.new('log/braintree.log')
-BraintreeRails::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
-BraintreeRails::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
-BraintreeRails::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
-
 Rails.configuration.to_prepare do
+  
+  Braintree::Configuration.environment = Rails.env.production? ? :production : :sandbox
+  Braintree::Configuration.logger = Logger.new('log/braintree.log')
+  Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
+  Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
+  Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
+  
   ChargeUser.client = Braintree::Gateway.new(
     environment: Rails.env.production? ? :production : :sandbox,
     merchant_id: ENV['BRAINTREE_MERCHANT_ID'],
